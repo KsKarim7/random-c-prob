7753f6e0e57b9cf0f1a3a1978c8a7de14401e921
Khaled Karim <kskarim177@gmail.com>
2023-04-06 11:20:56 +0600

    task 2

--- first-Pday.c
+++ first-Pday.c
@@ -8,3 +8,28 @@
 //     printf("Recently I heard that you've achieved 95% marks in your exam. \nThis is brilliant! \nI wish you'll shine in your life! \tGood luck all the barriers(^) in your life");
 //     return 0;
 // }
+
+// #2
+#include <stdio.h>
+int main()
+{
+    int var1, var2;
+    float resDiv;
+    int res;
+    scanf("%d %d", &var1, &var2);
+    // // printf("%d %d", var1 - var2);
+    res = var1 + var2;
+    printf("5 + 2 = ");
+    printf("%d ", res);
+    res = var1 - var2;
+    printf("\n5 - 2 = ");
+    printf("%d ", res);
+    res = var1 * var2;
+    printf("\n5 * 2 = ");
+    printf("%d ", res);
+    resDiv = var1 * 1.0 / var2;
+    printf("\n5 / 2 = ");
+    printf("%0.2f ", resDiv);
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+res = var1 - var2;
+    // printf("\n5 - 2 = ");
+    // printf("%d ", res);
+    // res = var1 * var2;
+    // printf("\n5 * 2 = ");
+    // printf("%d ", res);
