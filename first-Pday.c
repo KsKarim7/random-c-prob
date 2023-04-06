8df778a9853eafaba30ebed7ef61a6c8fa0cbe8b
Khaled Karim <kskarim177@gmail.com>
2023-04-06 09:35:43 +0600

    dun with the first problem

---
+++ first-Pday.c
@@ -0,0 +1,10 @@
+// Checkout the questions from here --
+// https://docs.google.com/document/d/19-yrrckK55JFnhlsc8OWG1LouTJ-DUkW2RUsKWIOT0c/edit
+
+// #1
+// #include <stdio.h>
+// int main()
+// {
+//     printf("Recently I heard that you've achieved 95% marks in your exam. \nThis is brilliant! \nI wish you'll shine in your life! \tGood luck all the barriers(^) in your life");
+//     return 0;
+// }
