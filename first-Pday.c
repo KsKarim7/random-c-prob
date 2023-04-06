8b88a6d735ba83691ee6dd31380c193fbf4458da
Khaled Karim <kskarim177@gmail.com>
2023-04-06 11:30:08 +0600

    task 4

--- first-Pday.c
+++ first-Pday.c
@@ -52,3 +52,24 @@
 //     }
 //     return 0;
 // }
+
+// task4
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    if (num < 0)
+    {
+        printf("negative");
+    }
+    else if (num > 0)
+    {
+        printf("positive");
+    }
+    else
+    {
+        printf("zero");
+    }
+    return 0;
+}
