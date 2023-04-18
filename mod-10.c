1cf5c5fdbcea4fd7e6603848a934be733e3c3ef0
Khaled Karim <kskarim177@gmail.com>
2023-04-18 15:00:30 +0600

    mod 10 dun

--- mod-10.c
+++ mod-10.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+// https://docs.google.com/document/d/16nJwleGtTHCab9XBs14X-7GUARQxVnGvbUVR1O9a9o8/edit
 int main()
 {
     // char s[1001], s1[1001];
@@ -8,10 +9,9 @@ int main()
     // int stt = strlen(s1);
     // printf("%d %d\n", st, stt);
     // printf("%s %s\n", s, s1);
-    char s[1000001];
-    // scanf("%s", &s);
-    fgets(s, 1000001, stdin);
-    int len = strlen(s);
+    // char s[1000001];
+    // fgets(s, 1000001, stdin);
+    // int len = strlen(s);
     // for (int i = 0; i < len; i++)
     // {
     //     if (s[i] == '\\')
@@ -23,9 +23,21 @@ int main()
     //         printf("%c", s[i]);
     //     }
     // }
-    for (int i = 0; s[i] != '\\'; i++)
+    // for (int i = 0; s[i] != '\\'; i++)
+    // {
+    //     printf("%c", s[i]);
+    // }
+
+    char s[1000001];
+    scanf("%s", &s);
+    int sum = 0;
+    for (int i = 0; i < strlen(s); i++)
     {
-        printf("%c", s[i]);
+        sum += (s[i] - 48);
+
+        // printf("%d\n", s[i] - 48);
     }
+    printf("%d", sum);
+
     return 0;
 }
